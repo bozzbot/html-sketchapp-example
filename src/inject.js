12e462094d42d745374c4bad6ff9b8558c0f4404
Enrico <enrico@serious.work>
2019-08-16 09:01:27 +0200

    Add scrolling for lazy loading images

--- src/inject.js
+++ src/inject.js
@@ -9,13 +9,57 @@ if (process.argv.length < 3) {
 const url = process.argv[2];
 const outputFile = '../page.asketch.json';
 
+function wait (ms) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+};
+
 puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
 
-  await page.setViewport({width: 1280, height: 800});
+  const vportDesktop = {
+    w: 1280,
+    h: 800
+  };
+  const vportMobile = {
+    w: 375,
+    h: 667
+  };
+
+  let vport = vportMobile;
+
+  await page.setViewport({width: vport.w, height: vport.h});
   await page.goto(url, {
     waitUntil: 'networkidle2'
   });
+
+  await wait(1000);
+
+  // Get the height of the rendered page
+  const bodyHandle = await page.$('body');
+  const { height } = await bodyHandle.boundingBox();
+  await bodyHandle.dispose();
+
+  await page.evaluate(() => {
+    // const viewPortHeight = document.documentElement.clientHeight;
+    let lastScrollTop = document.scrollingElement.scrollTop;
+    // Scroll to bottom of page until we can't scroll anymore.
+    const scroll = () => {
+      document.scrollingElement.scrollTop += 100;//(viewPortHeight / 2);
+      if (document.scrollingElement.scrollTop !== lastScrollTop) {
+        lastScrollTop = document.scrollingElement.scrollTop;
+        requestAnimationFrame(scroll);
+      }
+    };
+    scroll();
+  });
+
+  // Scroll back to top
+  await page.evaluate(_ => {
+    window.scrollTo(0, 0);
+  });
+
+  // Some extra delay to let images load
+  await wait(2000);
 
   await page.addScriptTag({
     path: './build/page2layers.bundle.js'
